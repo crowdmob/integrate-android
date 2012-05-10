6041725f5756e830f3f89b73ab3577e1e57b0113
Raj Shah <brainix@gmail.com>
2012-05-09 19:06:58 -0700

    started merging code from Android MobDeals into Installs (still doesn't work)

--- gen/com/crowdmob/installs/R.java
+++ gen/com/crowdmob/installs/R.java
@@ -11,13 +11,39 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int cancel_button=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+    }
+    public static final class id {
+        public static final int DialogWebView=0x7f05000d;
+        public static final int buttonBuyOffer=0x7f05000b;
+        public static final int buttonGpsOffer=0x7f050005;
+        public static final int buttonIpOffer=0x7f050004;
+        public static final int buttonOfferwall=0x7f050003;
+        public static final int cancelButton=0x7f05000e;
+        public static final int editApiKey=0x7f050001;
+        public static final int editApiSecret=0x7f050002;
+        public static final int layout_root=0x7f05000c;
+        public static final int textOfferCurrency=0x7f050008;
+        public static final int textOfferDiscount=0x7f050007;
+        public static final int textOfferExpires=0x7f05000a;
+        public static final int textOfferLocation=0x7f050009;
+        public static final int textOfferName=0x7f050006;
+        public static final int toggleSandbox=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int webviewdialog=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int buttonBuyOffer=0x7f040008;
+        public static final int buttonGpsOffer=0x7f040006;
+        public static final int buttonIpOffer=0x7f040007;
+        public static final int buttonOfferwall=0x7f040005;
+        public static final int editApiKeyHint=0x7f040003;
+        public static final int editApiSecretHint=0x7f040004;
         public static final int hello=0x7f040000;
+        public static final int toggleSandbox=0x7f040002;
     }
 }
